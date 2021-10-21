f12d4e59b629418deb4acbe5d41b4c6d64fcdaa0
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-10-22 03:14:27 +0900

    セーブデータの機能を作成した

---
+++ Assets/Scripts/Nakajima/SaveDataScripts/ISave.cs
@@ -0,0 +1,6 @@
+﻿
+public interface ISave
+{
+    void Save(SaveData.GameData data);
+    void Load(SaveData.GameData data);
+}
---
+++ Assets/Scripts/Nakajima/SaveDataScripts/LocalData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.IO;
+
+namespace AnoKara
+{
+    class LocalData
+    { 
+        public static void Save<T>(string file, T data)
+        {
+            StreamWriter writer;
+            var json = JsonUtility.ToJson(data);
+            writer = new StreamWriter(Application.dataPath + "/" +  file, false);
+            writer.Write(json);
+            writer.Flush();
+            writer.Close();
+        }
+
+        public static T Load<T>(string file)
+        {
+            string datastr;
+            StreamReader reader;
+
+            reader = new StreamReader(Application.dataPath + "/" + file);
+            datastr = reader.ReadToEnd();
+            reader.Close();
+
+            var gameData = JsonUtility.FromJson<T>(datastr); // ロードしたデータで上書き
+
+            if (gameData != null)
+            {
+                Debug.Log(gameData + "のデータをロードしました");
+                return gameData;
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
+
