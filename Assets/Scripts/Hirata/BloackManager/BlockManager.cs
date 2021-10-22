397b9c1766bcc9935b37be8ab4602f9a4d0b8078
HiraEi <aeblanket@gmail.com>
2021-10-22 19:56:18 +0900

    フォルダ整理と、ブロック崩しのシーンを追加

---
+++ Assets/Scripts/Hirata/BloackManager/BlockManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockManager : MonoBehaviour
+{
+    [SerializeField]
+    Button[] m_buttons;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
