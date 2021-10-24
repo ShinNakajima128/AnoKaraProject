9b91ec5e7553b9c559e91b76af0ad7c87687a729
HiraEi <aeblanket@gmail.com>
2021-10-24 22:18:52 +0900

    ブロック崩しの基本機能を追加した

---
+++ Assets/Scripts/Hirata/BloackManager/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ブロック崩しのボール
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class BallController : MonoBehaviour
+{
+    /// <summary>Rigidbody2D</summary>
+    Rigidbody2D m_rb2d;
+
+    /// <summary>開始した時にボールが飛んで行く方向</summary>
+    [SerializeField]
+    Vector2 m_startDirection = Vector2.up;
+
+    /// <summary>開始した時にボールにかける力</summary>
+    [SerializeField]
+    float m_startPowar = 3f;
+
+    /// <summary>失速判定になるスピード</summary>
+    [SerializeField]
+    float m_lowSpeed = 2f;
+
+    /// <summary>失速した時に付与するスピード</summary>
+    [SerializeField]
+    float m_addSpeed = 5f;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+        StartPush();
+    }
+
+    void Update()
+    {
+        if (m_rb2d.velocity.magnitude < m_lowSpeed)
+        {
+            m_rb2d.velocity = m_rb2d.velocity.normalized * m_addSpeed;
+        }
+    }
+
+    /// <summary>
+    /// ボールを飛ばす
+    /// </summary>
+    public void StartPush()
+    {
+        m_rb2d.AddForce(m_startDirection.normalized * m_startPowar, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/Hirata/BloackManager/PaddleController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PaddleController : MonoBehaviour
+{
+    /// <summary>Rigidbody2D</summary>
+    Rigidbody2D m_rb2d;
+
+    /// <summary>スピード</summary>
+    [SerializeField]
+    float m_speed = 5f;
+
+    /// <summary>右ボタンの長押しフラグ</summary>
+    bool m_isRightHold = false;
+
+    /// <summary>左ボタンの長押しフラグ</summary>
+    bool m_isLeftHold = false;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// 右ボタンが押された時(ボタンに設定)
+    /// </summary>
+    public void RightButtonDown()
+    {
+        m_isRightHold = true;
+    }
+
+    /// <summary>
+    /// 右ボタンが離された時(ボタンに設定)
+    /// </summary>
+    public void RightButtonUp()
+    {
+        m_isRightHold = false;
+    }
+
+    /// <summary>
+    /// 左ボタンが押された時(ボタンに設定)
+    /// </summary>
+    public void LeftButtonDown()
+    {
+        m_isLeftHold = true;
+    }
+
+    /// <summary>
+    /// 左ボタンが離された時(ボタンに設定)
+    /// </summary>
+    public void LeftButtonUp()
+    {
+        m_isLeftHold = false;
+    }
+
+    /// <summary>
+    /// パドルの移動処理
+    /// </summary>
+    void Move()
+    {
+        if (m_isRightHold && !m_isLeftHold)
+        {
+            m_rb2d.velocity = m_speed * Vector2.right;
+        }
+        else if (m_isLeftHold && !m_isRightHold)
+        {
+            m_rb2d.velocity = m_speed * Vector2.left;
+        }
+        else
+        {
+            m_rb2d.velocity = Vector2.zero;
+        }
+    }
+}
