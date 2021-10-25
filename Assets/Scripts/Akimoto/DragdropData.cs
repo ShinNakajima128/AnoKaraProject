2bdde54b63dee5f72b7536a816c006febd515b4f
AkimotoYuhi <chiisanaaki@gmail.com>
2021-10-25 09:49:21 +0900

    ドラッグドロップするミニゲーム仮実装

---
+++ Assets/Scripts/Akimoto/DragdropData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DragdropData : ScriptableObject
+{
+    public List<DragDropDataBase> m_database = new List<DragDropDataBase>();
+}
+
+[System.Serializable]
+public class DragDropDataBase
+{
+    /// <summary>目的</summary>
+    [SerializeField]
+    public string m_objective;
+
+    /// <summary>ドロップされる対象の画像</summary>
+    public Sprite m_dropImage;
+
+    /// <summary>ドラッグする画像</summary>
+    public Sprite[] m_dragImage;
+
+    /// <summary>下部のテキスト</summary>
+    public string[] m_texts;
+}
---
+++ Assets/Scripts/Akimoto/DropItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropItem : MonoBehaviour, IDropHandler
+{
+    //ドロップ=>正誤判定=>合ってたらオブジェクトを消してfaze++、間違ってたら何も起こらない
+
+    [SerializeField]
+    int[] m_clearNum = { 0, 1, 2 };
+
+    [SerializeField]
+    string[] m_text;
+
+    [SerializeField]
+    Text m_viewText;
+
+    private int m_faze = 0;
+
+    private void Start()
+    {
+        SetText();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        DragItem item = eventData.pointerDrag.GetComponent<DragItem>();
+        if (item.num == m_clearNum[m_faze])
+        {
+            item.gameObject.SetActive(false);
+            m_faze++;
+            SetText();
+            if (m_faze == m_clearNum.Length)
+            {
+                Debug.Log("クリア");
+            }
+        }
+    }
+
+    private void SetText()
+    {
+        Debug.Log(m_faze);
+        m_viewText.text = m_text[m_faze];
+    }
+}
