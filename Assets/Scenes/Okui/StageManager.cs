5973b875b8588297ff9c086a6cee7e179107cd7b
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-10-28 16:18:21 +0900

    ポップアップメニューでやめるを押すとアニメーションを逆再生するように実行

--- Assets/Scenes/Okui/StageManager.cs
+++ Assets/Scenes/Okui/StageManager.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor.Animations;
 
 public class StageManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
     GameObject panel;
-    
-   public void backButton()
+    [SerializeField]
+    Animator animator;
+    private void Start()
+    {
+        //animator = GetComponent<Animator>();
+    }
+    public void backButton()
     {
         Debug.Log("push");
         SceneManager.LoadScene("StudyPart");
     }
-   public void pushStage1()
+    public void pushStage1()
     {
+        animator.SetFloat("Speed", 1);
         panel.SetActive(true);
     }
-    
+    public void PopupCroze()
+    {
+        animator.SetFloat("Speed", -1);
+    }
+
 }
