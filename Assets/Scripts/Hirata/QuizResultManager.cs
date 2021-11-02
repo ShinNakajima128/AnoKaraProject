bb2da400cce88c6cff36d3284f4f25d58620aba5
HiraEi <aeblanket@gmail.com>
2021-11-02 13:12:40 +0900

    シーンとスクリプトを追加

---
+++ Assets/Scripts/Hirata/QuizResultManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>クイズのリザルトマネージャー</summary>
+public class QuizResultManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
