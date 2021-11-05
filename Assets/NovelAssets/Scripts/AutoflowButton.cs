b0cdeb408c28134d7a2e53ec00f6feb84b435c5d
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-11-05 19:48:20 +0900

    シナリオを再生する機能を持つAssetを追加した

---
+++ Assets/NovelAssets/Scripts/AutoflowButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoflowButton : MonoBehaviour
+{
+    Button m_autoButton = default; 
+    Text m_autoflowText = default;
+    Animator m_anim = default;
+
+    void Start()
+    {
+        m_autoButton = GetComponent<Button>();
+        m_autoflowText = transform.GetComponentInChildren<Text>();
+        m_anim = GetComponent<Animator>();
+        DialogManager.Instance.ContinueDialog += ContinueAnimation;
+        m_autoButton.onClick.AddListener(SwitchAutoflow);
+    }
+
+    public void SwitchAutoflow()
+    {
+        if (!DialogManager.Instance.IsAutoflow)
+        {
+            m_autoflowText.text = "オートON";
+            m_anim?.Play("ON");
+            DialogManager.Instance.IsAutoflow = true;
+        }
+        else
+        {
+            m_autoflowText.text = "オートOFF";
+            m_anim?.Play("OFF");
+            DialogManager.Instance.IsAutoflow = false;
+        }
+    }
+
+    void ContinueAnimation()
+    {
+        if (DialogManager.Instance.IsAutoflow)
+        {
+            m_autoflowText.text = "オートON";
+            m_anim?.Play("ON");
+        }
+    }
+}
---
+++ Assets/NovelAssets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogMasterData;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    CharacterData m_data = default;
+
+    void Start()
+    {
+        m_data = new CharacterData();
+
+        m_data.Talker = "みさき";
+        m_data.Messages = "aaaaaaaaa," +
+                          "bbbbbbb," +
+                          "ccccccc";
+    }
+}
