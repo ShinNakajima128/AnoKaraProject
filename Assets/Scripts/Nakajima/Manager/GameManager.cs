c8da1db4e4aa82a42f96157c61ca59b4ee17fbb1
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-11-06 20:27:03 +0900

    GameManagerのPrefabを作成した。テスト用に性別を選択できるようにした

--- Assets/Scripts/Nakajima/Manager/GameManager.cs
+++ Assets/Scripts/Nakajima/Manager/GameManager.cs
@@ -11,8 +11,13 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     [SerializeField]
     PeriodTypes m_currentPeriod = default;
 
+    [SerializeField]
+    GenderType m_playerGender = default;
+
     public int CurrentStageId { get => m_currentStageId; set => m_currentStageId = value; }
     public PeriodTypes CurrentPeriod { get => m_currentPeriod; set => m_currentPeriod = value; }
+
+    public GenderType PlayerGender => m_playerGender;
 
     private void Awake()
     {
