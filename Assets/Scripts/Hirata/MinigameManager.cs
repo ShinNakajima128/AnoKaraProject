8c5bfd459926050e45a976b726665dd3f2acf4f6
HiraEi <aeblanket@gmail.com>
2021-11-08 15:56:00 +0900

    ミニゲームマネージャーを作成

---
+++ Assets/Scripts/Hirata/MinigameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>ミニゲーム管理クラス</summary>
+public class MinigameManager : MonoBehaviour
+{
+    /// <summary>
+    /// ミニゲームの種類
+    /// </summary>
+    enum Minigame
+    {
+        /// <summary>ブロック崩し</summary>
+        Block,
+        /// <summary>選択型</summary>
+        Choice,
+        /// <summary>回転型</summary>
+        Spin,
+    }
+
+    /// <summary>ミニゲーム</summary>
+    Minigame m_minigame;
+
+    /// <summary>
+    /// ミニゲームを設定し、該当シーンに移動する
+    /// ボタンに設定する
+    /// </summary>
+    /// <param name="minigame">呼びたいミニゲーム</param>
+    public void MinigameButton(string minigame)
+    {
+        if (minigame == "Block")
+        {
+            m_minigame = Minigame.Block;
+        }
+        else if (minigame == "Choice")
+        {
+            m_minigame = Minigame.Choice;
+        }
+        else if (minigame == "Spin")
+        {
+            m_minigame = Minigame.Spin;
+        }
+
+        LoadSceneManager.AnyLoadScene(m_minigame.ToString(), () =>
+        {
+            LoadSceneManager.FadeOutPanel();
+        });
+    }
+}
