7fc9c5ce773cac61927275b73fff118f8fd4742f
HiraEi <aeblanket@gmail.com>
2021-11-12 14:47:52 +0900

    UIの整理と、移動制限の試作をした

--- Assets/Scripts/Hirata/PeriodSelectManager.cs
+++ Assets/Scripts/Hirata/PeriodSelectManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 /// <summary>時代選択画面マネージャー</summary>
-public class PeriodSelectManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
+public class PeriodSelectManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     /// <summary>ドラッグしているか否か</summary>
     bool m_isDrag = false;
@@ -15,16 +15,31 @@ public class PeriodSelectManager : MonoBehaviour, IBeginDragHandler, IDragHandle
     /// <summary>ドラッグ開始時のUIのポジション</summary>
     Vector2 m_startPosition;
 
-    /// <summary></summary>
-    [SerializeField]
-    GameObject m_stopLeft;
+    RectTransform m_rectTransform;
 
-    [SerializeField]
-    GameObject m_stopRight;
+    Vector3[] m_fourCorners = new Vector3[4];
+
+    float depth = -1f;
+
+    Vector3 rightTop;
+
+    Vector3 leftBottom;
 
     private void Start()
     {
+        m_rectTransform = GetComponent<RectTransform>();
+        GetFourConers();
+        rightTop = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, depth));
+        leftBottom = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, depth));
+    }
 
+    void GetFourConers()
+    {
+        m_rectTransform.GetWorldCorners(m_fourCorners);
+        //foreach (var item in m_fourCorners)
+        //{
+        //    Debug.Log(item);
+        //}
     }
 
     /// <summary>
@@ -43,9 +58,15 @@ public class PeriodSelectManager : MonoBehaviour, IBeginDragHandler, IDragHandle
     /// </summary>
     public void OnDrag(PointerEventData eventData)
     {
-        Vector2 vector = eventData.position - m_pointerPosition;
-        vector.y = 0;
-        transform.position = m_startPosition + vector;
+        GetFourConers();
+        Debug.Log(m_fourCorners[0].x);
+        Debug.Log(leftBottom.x);
+        if (m_fourCorners[0].x < leftBottom.x)
+        {
+            Vector2 vector = eventData.position - m_pointerPosition;
+            vector.y = 0;
+            transform.position = m_startPosition + vector;
+        }
         Debug.Log("ドラッグ中");
     }
 
@@ -58,20 +79,17 @@ public class PeriodSelectManager : MonoBehaviour, IBeginDragHandler, IDragHandle
         Debug.Log("ドラッグ終了");
     }
 
+    
     /// <summary>
-    /// クリックした処理
+    /// 選択された時代に移動する
     /// </summary>
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (m_isDrag)
-        {
-            return;
-        }
-        Debug.Log("タッチ");
-    }
-
-    public void Button()
+    /// <param name="period">時代のシーン名</param>
+    public void PeriodButton(string period)
     {
         Debug.Log("押された");
+        //LoadSceneManager.AnyLoadScene(period, () =>
+        //{
+        //    LoadSceneManager.FadeOutPanel();
+        //});
     }
 }
