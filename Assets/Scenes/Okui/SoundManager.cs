53a5f4ddbfdb92d7d994bdda7260a133f58de379
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-11-12 18:22:39 +0900

    サウンドマネージャー作成開始

---
+++ Assets/Scenes/Okui/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+/// <summary>
+/// 音源管理
+/// </summary>
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager sound;
+
+   /// <summary>
+   /// BGN管理
+   /// </summary>
+    public enum BGM_Type
+    {
+        BGM,
+        BGM2,
+        BGM3,
+    }
+    //SE管理
+    public enum SE_Type
+    {
+        SE,
+        SE2,
+        SE3,
+    }
+    /// <summary>AudioClip</summary>
+
+    public AudioClip[] BGM_Clips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
