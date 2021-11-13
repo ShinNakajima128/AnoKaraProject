ae8b60563d7ff40a9f4b68b7f7896adc5d8b002f
HiraEi <aeblanket@gmail.com>
2021-11-13 16:17:54 +0900

    ステージが選択されたことがわかるようにした

--- Assets/Scripts/Hirata/PeriodSelectManager.cs
+++ Assets/Scripts/Hirata/PeriodSelectManager.cs
@@ -14,12 +14,27 @@ public class PeriodSelectManager : MonoBehaviour
 
     /// <summary>ステージ選択画面のキャラクターを表示するイメージ</summary>
     [SerializeField]
-    Image m_stageCharaImage;
+    Image m_stageCharactorImage;
+
+    /// <summary>ステージ選択のボタン</summary>
+    [SerializeField]
+    Button[] m_stageButtons;
+
+    /// <summary>ステージ選択のテキスト</summary>
+    [SerializeField]
+    Text[] m_stageText;
+
+    /// <summary>ボタンが選択されたフレーム</summary>
+    [SerializeField]
+    GameObject[] m_stageSelectImage;
+
+    /// <summary>選択されたステージ番号の保存</summary>
+    int m_selectedStageNum;
 
     /// <summary>
     /// ステージ選択のPanelをアクティブにする
     /// </summary>
-    public void SelectStage(int period)
+    public void SelectPeriod(int period)
     {
         SetPeriod(period);
         m_stageSelectPanel.SetActive(true);
@@ -35,11 +50,43 @@ public class PeriodSelectManager : MonoBehaviour
     }
 
     /// <summary>
+    /// ステージボタンが選択された処理
+    /// ボタンに設定する
+    /// </summary>
+    public void SelectStage(int stage)
+    {
+        m_stageSelectImage[m_selectedStageNum].gameObject.SetActive(false);
+        m_selectedStageNum = stage - 1;
+        m_stageSelectImage[stage - 1].gameObject.SetActive(true);
+        SetStage(stage);
+    }
+
+    /// <summary>
+    /// ステージ番号をゲームマネージャーに設定する
+    /// </summary>
+    /// <param name="stageNum">ステージ番号</param>
+    void SetStage(int stageNum)
+    {
+        GameManager.Instance.CurrentStageId = stageNum;
+    }
+
+    /// <summary>
     /// 時代選択に戻る
+    /// ボタンに設定する
     /// </summary>
     public void BackSelectPeriod()
     {
         GameManager.Instance.CurrentPeriod = (MasterData.PeriodTypes)0;
         m_stageSelectPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// シーン移動
+    /// 決定ボタンに設定する
+    /// </summary>
+    /// <param name="scene"></param>
+    public void Decision(string scene)
+    {
+        LoadSceneManager.AnyLoadScene(scene);
     }
 }
