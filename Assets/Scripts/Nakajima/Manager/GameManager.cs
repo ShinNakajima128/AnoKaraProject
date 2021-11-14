3ab68af4f0916f5c35b6eb675970793f0143044b
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-11-15 02:44:28 +0900

    GameManagerのテスト用に使用していたコードが要らなくなったので消去した

--- Assets/Scripts/Nakajima/Manager/GameManager.cs
+++ Assets/Scripts/Nakajima/Manager/GameManager.cs
@@ -26,13 +26,8 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     [SerializeField]
     int m_currentStageId = default;
 
-    /// <summary> プレイヤーの性別 </summary>
-    [SerializeField]
-    GenderType m_playerGender = default;
-
     public int CurrentStageId { get => m_currentStageId; set => m_currentStageId = value; }
     public PeriodTypes CurrentPeriod { get => m_currentPeriod; set => m_currentPeriod = value; }
-    public GenderType PlayerGender => m_playerGender;
 
     private void Awake()
     {
