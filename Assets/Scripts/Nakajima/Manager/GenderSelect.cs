30d1dd47a958e1d7b2a6b012b83a68b715ad1d33
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-11-14 21:55:27 +0900

    タイトル画面で性別を指定する機能を作成した

---
+++ Assets/Scripts/Nakajima/Manager/GenderSelect.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenderSelect : MonoBehaviour
+{
+    [SerializeField]
+    Image[] m_genderImages = default;
+
+    [SerializeField]
+    GameObject m_confirmPanel = default;
+
+    GenderType m_selectGender = default;
+
+    private void Start()
+    {
+        m_confirmPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// 性別を選択する
+    /// </summary>
+    /// <param name="Type"> 性別の種類 </param>
+    public void SelectGender(int Type)
+    {
+        //Boyを選択した場合、Girlを暗転して仮でBoyを変数にセットする
+        if (Type == 0)
+        {
+            m_genderImages[1].color = new Color(0.2f, 0.2f, 0.2f);
+            m_selectGender = GenderType.Boy;
+        }
+        //Girlを選択した場合、Boyを暗転して仮でGirlを変数にセットする
+        else
+        {
+            m_genderImages[0].color = new Color(0.3f, 0.3f, 0.3f);
+            m_selectGender = GenderType.Girl;
+        }
+        //確認画面を表示する
+        m_confirmPanel.SetActive(true);
+    }
+
+    public void Submit()
+    {
+        m_confirmPanel.SetActive(false);
+        TitleManager.Instance.TempGender = m_selectGender;
+        TitleManager.Instance.ChangePanel(TitleStates.InputName);
+    }
+
+    public void Cancel()
+    {
+        m_confirmPanel.SetActive(false);
+        foreach (var i in m_genderImages)
+        {
+            i.color = new Color(1f, 1f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Nakajima/Manager/TitleManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum TitleStates
+{
+    Start,
+    SelectGender,
+    InputName,
+    FinalConfirm
+}
+
+public class TitleManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    TitleStates m_titleState = TitleStates.Start;
+
+    [Header("タイトル画面の各Panel")]
+    [SerializeField]
+    GameObject m_startPanel = default;
+
+    [SerializeField]
+    GameObject m_initialSettingPanel = default;
+
+    [SerializeField]
+    GameObject[] m_settingPanels = default;
+
+    [SerializeField]
+    GameDataObject gameDataObject = default;
+
+    PlayerData m_playerData = default;
+
+    public static TitleManager Instance { get; private set; }
+
+    public string TempPlayerName { get; set; }
+    public GenderType TempGender { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        ChangePanel(TitleStates.Start);
+    }
+
+    /// <summary>
+    /// Panelを切り替える
+    /// </summary>
+    /// <param name="States"> 切り替えるPanelの索引 </param>
+    public void ChangePanel(TitleStates States)
+    {
+        m_titleState = States;
+
+        switch (m_titleState)
+        {
+            case TitleStates.Start:
+                m_startPanel.SetActive(true);
+                m_initialSettingPanel.SetActive(false);
+                break;
+            case TitleStates.SelectGender:
+                m_startPanel.SetActive(false);
+                m_initialSettingPanel.SetActive(true);
+                for (int i = 0; i < m_settingPanels.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        m_settingPanels[i].SetActive(true);
+                    }
+                    else
+                    {
+                        m_settingPanels[i].SetActive(false);
+                    }
+                }
+                break;
+            case TitleStates.InputName:
+                m_startPanel.SetActive(false);
+                m_initialSettingPanel.SetActive(true);
+                for (int i = 0; i < m_settingPanels.Length; i++)
+                {
+                    if (i == 1)
+                    {
+                        m_settingPanels[i].SetActive(true);
+                    }
+                    else
+                    {
+                        m_settingPanels[i].SetActive(false);
+                    }
+                }
+                break;
+            case TitleStates.FinalConfirm:
+
+                break;
+        }
+    }
+
+
+    public void ChangPanelByButton(int type)
+    {
+
+    }
+    /// <summary>
+    /// ゲームを開始する
+    /// </summary>
+    public void GameStart()
+    {
+        SaveAndLoadTest.Instance.LoadData();
+        m_playerData = DataManager.Instance.PlayerData;
+
+        //プレイヤーデータがあれば時代選択画面に遷移する
+        if (m_playerData.PlayerName != "")
+        {
+            Debug.Log($"プレイヤー名：{m_playerData.PlayerName}、性別：{m_playerData.PlayerGender}");
+
+            LoadSceneManager.AnyLoadScene("時代選択画面", () =>
+            {
+                Debug.Log("時代選択画面に遷移します");
+            });
+        }
+        //プレイヤーデータが無ければ初期設定画面を表示
+        else
+        {
+            ChangePanel(TitleStates.SelectGender);
+        }
+    }
+}
