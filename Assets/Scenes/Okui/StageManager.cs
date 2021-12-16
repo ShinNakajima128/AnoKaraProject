b99991cbcf3d845f55dff143147a5cbfbf3e6021
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-12-16 23:45:10 +0900

    ビルドテスト時に起きていたエラーの原因の対処を行った

--- Assets/Scenes/Okui/StageManager.cs
+++ Assets/Scenes/Okui/StageManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 
 public class StageManager : MonoBehaviour
 {
