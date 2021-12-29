ccb82cb474646b032535db5ebb4583f1815d7569
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-12-29 23:52:11 +0900

    セーブデータを管理する処理を修正した

--- Assets/Scripts/Nakajima/SaveDataScripts/LocalData.cs
+++ Assets/Scripts/Nakajima/SaveDataScripts/LocalData.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.IO;
+using UnityEngine.Networking;
 
 namespace AnoKara
 {
     class LocalData
-    { 
+    {
         /// <summary>
         /// ローカルファイルにデータを保存する
         /// </summary>
@@ -13,23 +14,15 @@ namespace AnoKara
         /// <param name="data"> 現在のデータ </param>
         public static void Save<T>(string file, T data)
         {
-            StreamWriter writer;
             var json = JsonUtility.ToJson(data);
-            writer = new StreamWriter(Application.dataPath + "/" +  file, false);
-            writer.Write(json);
-            writer.Flush();
-            writer.Close();
+            PlayerPrefs.SetString(file, json);
         }
 
         public static T Load<T>(string file)
         {
             string datastr;
-            StreamReader reader;
-
-            reader = new StreamReader(Application.dataPath + "/" + file);
-            datastr = reader.ReadToEnd();
-            reader.Close();
-
+            datastr = PlayerPrefs.GetString(file);
+            
             var gameData = JsonUtility.FromJson<T>(datastr); // ロードしたデータで上書き
 
             if (gameData != null)
