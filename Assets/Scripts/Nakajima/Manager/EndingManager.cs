ac3fb8b5d89920e2c859d8f8f448eaa82b693917
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-01-06 18:03:12 +0900

    EndingSceneを作成し、エンディングのシナリオデータを再生できるようにした

---
+++ Assets/Scripts/Nakajima/Manager/EndingManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingManager : MonoBehaviour
+{
+    [Header("タイトル画面のScene名")]
+    [SerializeField]
+    string m_titleSceneName = default;
+
+    private void Start()
+    {
+        EventManager.ListenEvents(Events.FinishDialog, LoadPeriodSelectScene);
+        SoundManager.Instance.PlayBgm(SoundManager.Instance.BgmName);
+    }
+    /// <summary>
+    /// 時代選択画面に遷移する
+    /// </summary>
+    void LoadPeriodSelectScene()
+    {
+        LoadSceneManager.AnyLoadScene(m_titleSceneName);
+    }
+}
