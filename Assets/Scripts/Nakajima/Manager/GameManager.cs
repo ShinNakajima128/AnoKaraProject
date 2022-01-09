7a3642b5bacc79795c510d536cdc0f614739c1bc
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-01-09 17:17:19 +0900

    GameManagerに探索パートでのクイズ前か後かどうかのフラグを追加

--- Assets/Scripts/Nakajima/Manager/GameManager.cs
+++ Assets/Scripts/Nakajima/Manager/GameManager.cs
@@ -25,6 +25,9 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     public int CurrentStageId { get => m_currentStageId; set => m_currentStageId = value; }
     public PeriodTypes CurrentPeriod { get => m_currentPeriod; set => m_currentPeriod = value; }
 
+    /// <summary> クイズシーンから戻っていたかどうかのフラグ </summary>
+    public bool IsAfterQuized { get; set; } = false;
+
     private void Awake()
     {
         if (Instance != this)
